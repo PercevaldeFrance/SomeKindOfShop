dcb5ec5fc0a1d64939e5d7f358b3f82baddec0f9
PercevaldeFrance <70976301+PercevaldeFrance@users.noreply.github.com>
2022-07-12 22:01:50 +0300

    shop is going further

---
+++ SomeKindOfShop/ItemsLibrary.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomeKindOfShop
+{
+    public class ItemsLibrary
+    {
+        public abstract class Cloth :Item
+        {
+            public int Armor { get; set; }
+        }
+        public abstract class Weapon: Item
+        {
+            public int Damage { get; set; }
+        }
+        public abstract class Meal : Item 
+        {
+            public int Tastyness { get; set; }
+        }
+        
+        public abstract class Item
+        {
+            public int price;
+            public int weight;
+        }
+
+        public class Shoes : Cloth
+        {
+            public Shoes()
+            {
+                price = 25;
+                Armor = 10;
+            }
+        }
+        public class Cape : Cloth
+        {
+            public Cape()
+            {
+                price = 30;
+                Armor = 10;
+                
+            }
+        }
+        public class Pants : Cloth
+        {
+            public Pants()
+            {
+                price = 15;
+                Armor = 10;
+            }
+        }
+        public class Sword : Weapon
+        {
+            public Sword()
+            {
+                price = 50;
+                Damage = 25;
+            }
+        }
+        public class Bow : Weapon
+        {
+            public Bow()
+            {
+                price = 40;
+                Damage = 20;
+            }
+        }
+        public class Axe : Weapon
+        {
+            public Axe()
+            {
+                price = 60;
+                Damage = 50;
+            }
+        }
+        public class Potion : Meal
+        {
+            public Potion()
+            {
+                price = 5;
+                Tastyness = 2;
+            }
+        }
+        public class Bread : Meal
+        {
+            public Bread()
+            {
+                price = 1;
+                Tastyness = 10;
+            }
+        }
+        public class Gluwein : Meal
+        {            
+            public Gluwein()
+            {
+                price = 10;
+                Tastyness = 50;
+            }
+        }
+    }
+}
--- SomeKindOfShop/Program.cs
+++ SomeKindOfShop/Program.cs
@@ -2,74 +2,20 @@
 
 namespace SomeKindOfShop
 {
-    interface IWearable
-    {
-        void Wear();
-    }
-    interface IDamageable
-    {
-        void Damage();
-    }
-    public class Items
-    {
-        public int price;
-        public int armor;
-        public int speed;
-        public int damage;
-    }
-
-    public class Shoes : Items, IWearable 
-    {
-        public Shoes()
-        {
-            price = 25;
-            armor = 3;
-            speed = 20;
-        }
-    }
-    class Cape : Items, IWearable
-    {
-        public Cape()
-        {
-            price = 30;
-            armor = 10;
-            speed = 2;
-        }
-    }
-    class Pants : Items, IWearable
-    {
-        public Pants()
-        {
-            price = 15;
-            armor = 10;
-            speed = 10;
-        }
-    }
-    class Sword : Items, IDamageable
-    {
-        public Sword()
-        {
-            price = 50;
-            damage = 25;
-        }
-    }
-    class Bow : Items, IDamageable
-    {
-        public Bow()
-        {
-            price = 40;
-            damage = 20;
-        }
-    }
+ 
     class Program
     {
         static void Main(string[] args)
-        {
-            Shoes shoes = new Shoes();
-            Cape cape = new Cape();
-            Pants pants = new Pants();
-            Sword sword = new Sword();
-            Bow bow = new Bow();
+        {    
+            ItemsLibrary.Shoes shoes = new ItemsLibrary.Shoes();
+            ItemsLibrary.Cape cape = new ItemsLibrary.Cape();
+            ItemsLibrary.Pants pants = new ItemsLibrary.Pants();
+            ItemsLibrary.Sword sword = new ItemsLibrary.Sword();
+            ItemsLibrary.Bow bow = new ItemsLibrary.Bow();
+            ItemsLibrary.Axe axe = new ItemsLibrary.Axe();
+            ItemsLibrary.Potion potion = new ItemsLibrary.Potion();
+            ItemsLibrary.Bread bread = new ItemsLibrary.Bread();
+            ItemsLibrary.Gluwein gluwein = new ItemsLibrary.Gluwein();
 
         }
     }
